b5c5e40e797a6e8bd938e6024ac9622c06106e20
jdubra <92731151+jdubra@users.noreply.github.com>
2023-02-22 10:30:35 -0300

    Update src/helpers/styleToString.tsx
    Co-authored-by: neocoaster <32307271+neocoaster@users.noreply.github.com>

--- src/helpers/styleToString.tsx
+++ src/helpers/styleToString.tsx
@@ -1,6 +1,7 @@
 const styleToString = (style: React.CSSProperties) => {
   let str = '';
   const properties = Object.entries(style);
+  
   properties.forEach((property) => {
     str += `${property[0]}: ${property[1]}; `;
   });
